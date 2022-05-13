5e25d4080170cf355d1fee42ebc913e8b63e668b
SuperGandhi <patrickborjabueno@hotmail.com>
2022-05-13 13:42:59 -0500

    refactoring

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
-import React from "react";
+
+import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
-import 'antd/dist/antd.min.css';
+
+import App from './App';
+import store from './app/store';
+
+import 'antd/dist/antd.css';
 
 ReactDOM.render(
-  <Router>
-    <App/> 
-  </Router>,
-  document.getElementById('root'));
+  <React.StrictMode>
+    <Router>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
+
